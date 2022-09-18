d209b8c21407c92078fa55fcf775840947875848
Sergey Kislov <serejka1991@gmail.com>
2022-09-18 12:21:34 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int value = new Scanner(System.in).nextInt();
+
+        int result = 1;
+
+        int i = 0;
+        int o = 0;
+        for (i = 6; i > 0; i--) {
+            for (o = 1; o <= value; o++) {
+                if (i * o == value) {
+                    System.out.println(i + " * " + o);
+                }
+            }
+        }
+
+
+
+    }
+}
